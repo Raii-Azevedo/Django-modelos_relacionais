bbeabf94df40e1c198fc64afd67db18431ffdf2c
Raissa Azevedo <rhaii.azevedo@gmail.com>
2021-04-28 18:46:07 -0300

    Update settings.py

--- djangoorm/settings.py
+++ djangoorm/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3s=tx5*=vol5un6xr4f!vp62vl(h_b27x91-e9octm3pybmu1^'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
